b5e6611696eb33394d0a2b32115e8d7bd837de00
MaxGolden <maxgold226@gmail.com>
2019-04-06 21:47:58 -0500

    whitenoise upload

--- README.md
+++ README.md
@@ -1,15 +1,22 @@
-## Test
-Aaa
+# <img src='https://raw.githack.com/FortAwesome/Font-Awesome/master/svgs/solid/laugh-beam.svg' card_color='#22a7f0' width='50' height='50' style='vertical-align:bottom'/> Whitenoise
+Mycroft whitenoise
 
-## Description
-Aaaaaaaaa
+## About 
+Mycroft will play the whitenoise by using text to speech. 
 
-## Examples
- - "Wq"
- - "Wq"
+## Examples 
+* "play some whitenoise"
 
-
-## Credits
+## Credits 
 max
 
+## Category
+**Entertainment**
 
+## Tags
+#sing
+#singing
+#lyrics
+#song
+#music
+#texttospeech
--- __init__.py
+++ __init__.py
@@ -1,15 +1,46 @@
-from mycroft import MycroftSkill, intent_file_handler
+import random
+from os.path import dirname, join
+
+from adapt.intent import IntentBuilder
+from mycroft import MycroftSkill, intent_handler
+from mycroft.skills.audioservice import AudioService
+from mycroft.audio import wait_while_speaking
 
 
-class Test(MycroftSkill):
+class WhitenoiseSkill(MycroftSkill):
     def __init__(self):
-        MycroftSkill.__init__(self)
+        super(WhitenoiseSkill, self).__init__(name="WhitenoiseSkill")
+        self.process = None
+        self.play_list = {
+            0: join(dirname(__file__), "popey-whitenoise.mp3"),
+            1: join(dirname(__file__), "popey-whitenoiseocean.mp3"),
+            2: join(dirname(__file__), "popey-whitenoiserain.mp3"),
+            3: join(dirname(__file__), "popey-whitenoisewave.mp3"),
+        }
 
-    @intent_file_handler('test.intent')
-    def handle_test(self, message):
-        self.speak_dialog('test')
+    def initialize(self):
+        self.audioservice = AudioService(self.bus)
+        self.add_event("mycroft.whitenoise", self.whitenoise, False)
+
+    def whitenoise(self, message):
+        self.process = play_mp3(self.play_list[0])
+
+    @intent_handler(IntentBuilder('').require('Whitenoise'))
+    def handle_whitenoise(self, message):
+        path = random.choice(self.play_list)
+        try:
+            self.speak_dialog('singing')
+            wait_while_speaking()
+            self.audioservice.play(path)
+        except Exception as e:
+            self.log.error("Error: {0}".format(e))
+
+    def stop(self):
+        if self.process and self.process.poll() is None:
+            self.speak_dialog('whitenoise.stop')
+            self.process.terminate()
+            self.process.wait()
 
 
 def create_skill():
-    return Test()
-
+    return WhitenoiseSkill()
